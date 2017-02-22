0948b11dc1f3730174d5559021fef267aaa34683
Isao-Shinohara <shinohara@double-rabbits.net>
2017-02-23 08:53:04 +0900

    First commit.

---
+++ src/main/java/net/double_rabbits/TimeDeadLine_Spring/TimeDeadLineSpringApplication.java
@@ -0,0 +1,12 @@
+package net.double_rabbits.TimeDeadLine_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeDeadLineSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeDeadLineSpringApplication.class, args);
+	}
+}
